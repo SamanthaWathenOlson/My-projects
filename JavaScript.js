3257703370f50a5533430805ef0be48546afcdc3
Samantha WathenOlson <samantha.wathen269@gmail.com>
2022-02-08 01:29:21 -0500

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,17 @@
+Function makeSalad(topping1, topping2, topping3) {
+    salad = 'lettuce' + ','
+    salad += topping1 + ','
+    salad += topping2 + ','
+    salad += topping3 + ','
+    salad += 'lettuce' 
+    return salad
+}
 
+result = makeSalad('tomato', 'radish', 'cheese')
+
+
+if (mode === 'public') {
+showDetails();
+} else {
+hideDetails();
+}
