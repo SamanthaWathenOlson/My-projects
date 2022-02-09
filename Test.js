49cceb226d62f8b4601fee242e02ab554e5f40ff
Samantha WathenOlson <samantha.wathen269@gmail.com>
2022-02-09 17:10:19 -0500

    Update Test.js

--- Test.js
+++ Test.js
@@ -43,3 +43,19 @@ public void setaddress(String address) {
  system.out.println(s.getName());
  }
  }
+public class Test {
+
+   public static void main(String args[]) {
+      int x = 30;
+
+      if( x == 10 ) {
+         System.out.print("Value of X is 10");
+      }else if( x == 20 ) {
+         System.out.print("Value of X is 20");
+      }else if( x == 30 ) {
+         System.out.print("Value of X is 30");
+      }else {
+         System.out.print("This is else statement");
+      }
+   }
+}
