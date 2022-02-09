22a638552ca385c3c66276489b17da573d543ed2
Samantha WathenOlson <samantha.wathen269@gmail.com>
2022-02-09 18:33:48 -0500

    Update static.js

--- static.js
+++ static.js
@@ -94,3 +94,24 @@ public class MammalInt implements Animal {
       m.travel();
    }
 } 
+
+// Filename: Sports.java
+public interface Sports {
+   public void setHomeTeam(String name);
+   public void setVisitingTeam(String name);
+}
+
+// Filename: Football.java
+public interface Football extends Sports {
+   public void homeTeamScored(int points);
+   public void visitingTeamScored(int points);
+   public void endOfQuarter(int quarter);
+}
+
+// Filename: Hockey.java
+public interface Hockey extends Sports {
+   public void homeGoalScored();
+   public void visitingGoalScored();
+   public void endOfPeriod(int period);
+   public void overtimePeriod(int ot);
+}
