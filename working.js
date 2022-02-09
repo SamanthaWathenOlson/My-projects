8725d1fffe942d6f66cd72045c93b37609078e20
Samantha WathenOlson <samantha.wathen269@gmail.com>
2022-02-09 17:08:23 -0500

    Create working.js

---
+++ working.js
@@ -0,0 +1,22 @@
+public class Test {
+
+   public static void main(String args[]) {
+      int x = 10;
+
+      if( x < 20 ) {
+         System.out.print("This is if statement");
+      }
+   }
+}
+public class Test {
+
+   public static void main(String args[]) {
+      int x = 30;
+
+      if( x < 20 ) {
+         System.out.print("This is if statement");
+      }else {
+         System.out.print("This is else statement");
+      }
+   }
+}
