9cac2b4e5fafd705dd937ba132b490ff3f1fa647
Samantha WathenOlson <samantha.wathen269@gmail.com>
2022-02-09 18:06:29 -0500

    Update bank.js

--- bank.js
+++ bank.js
@@ -40,3 +40,20 @@ public static void main(String args[]){
 Dog d=new Dog();  
 d.printColor();  
 }}
+
+class Animal{  
+void eat(){System.out.println("eating...");}  
+}  
+class Dog extends Animal{  
+void eat(){System.out.println("eating bread...");}  
+void bark(){System.out.println("barking...");}  
+void work(){  
+super.eat();  // Calling the parent class eat() method using super keyword
+bark();  
+}  
+}  
+class TestSuper2{  
+public static void main(String args[]){  
+Dog d=new Dog();  
+d.work();  
+}}
