3d70f92564ec64d592162a80535a5f556fa4e9b9
Samantha WathenOlson <samantha.wathen269@gmail.com>
2022-02-09 17:16:32 -0500

    Update working.js

--- working.js
+++ working.js
@@ -1,4 +1,7 @@
-public class Test {
+
+if(Boolean_expression) {
+   // Statements will execute if the Boolean expression is true
+}public class Test {
 
    public static void main(String args[]) {
       int x = 10;
@@ -7,6 +10,11 @@ public class Test {
          System.out.print("This is if statement");
       }
    }
+}
+if(Boolean_expression) {
+   // Executes when the Boolean expression is true
+}else {
+   // Executes when the Boolean expression is false
 }
 public class Test {
 
@@ -17,6 +25,25 @@ public class Test {
          System.out.print("This is if statement");
       }else {
          System.out.print("This is else statement");
+      }
+   }
+}
+if(Boolean_expression 1) {
+   // Executes when the Boolean expression 1 is true
+   if(Boolean_expression 2) {
+      // Executes when the Boolean expression 2 is true
+   }
+}
+public class Test {
+
+   public static void main(String args[]) {
+      int x = 30;
+      int y = 10;
+
+      if( x == 30 ) {
+         if( y == 10 ) {
+            System.out.print("X = 30 and Y = 10");
+         }
       }
    }
 }
